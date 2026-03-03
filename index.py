5151a1f32911b6856615037ade3bf911b4c2f026
guilherme <guilhermemotta043@gmail.com>
2026-03-03 14:25:17 -0300

    primeiros-passos
    Aqui adicionei o container branco e inseri um único número à calculadora.
    
    importei a biblioteca tk, criando a janela e botão

--- index.py
+++ index.py
@@ -5,4 +5,8 @@ janela.title('Calculadora Louca')
 
 janela.geometry('300x400')
 
+display = tk.Entry(janela, font=('Arial', 20), justify= 'right')
+
+botao1= tk.Button(janela, text=('1'), font=('Arial', 18))
+
 janela.mainloop()
