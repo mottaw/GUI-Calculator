1047d7b3a14807561906056942457e1905fcff95
guilherme <guilhermemotta043@gmail.com>
2026-03-04 14:08:22 -0300

    update do index
    coloquei mais botões com loop dentro de loop, fazendo com que as colunas e linhas se organizem com a função "GRID".
    Erro de cada botão digitando o algarismo 9 foi corrigido declarando a n=num dentro do lambda

--- index.py
+++ index.py
@@ -1,12 +1,33 @@
 import tkinter as tk
 
+def clickar(numero):
+    display.insert(tk.END, numero)
+    
+
 janela = tk.Tk()
 janela.title('Calculadora Louca')
 
-janela.geometry('300x400')
+janela.geometry('320x400')
 
 display = tk.Entry(janela, font=('Arial', 20), justify= 'right')
+display.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-botao1= tk.Button(janela, text=('1'), font=('Arial', 18))
+num = 1
+
+for linha in range(3, 0, -1):
+    for coluna in range(3):
+        botao = tk.Button(
+            janela,
+            text=(num),
+            font=('Arial', 18),
+            width=6,
+            height=1,
+            command=lambda n=num: clickar(n)
+        )
+        botao.grid(row=4-linha, column=coluna, padx=5, pady=5)
+        num += 1
+
+botao0 = tk.Button(janela, text="0", font=('Arial', 18), width=5, command=lambda: clickar(0))
+botao0.grid(row=4, column=1, padx=5, pady=5)   
 
 janela.mainloop()
